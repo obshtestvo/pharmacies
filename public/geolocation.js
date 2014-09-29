17e83d50d8ab01262149ea7020bc64ca14d5a424
Joro Penchev <joro.penchev@gmail.com>
2014-09-29 10:45:27 +0300

    Finished pharmacies fetch with predefined distance

--- public/geolocation.js
+++ public/geolocation.js
@@ -1,44 +1,36 @@
 $(function() {
 
-	// var data = {
-	//     resource_id: '3d38a652-adbc-442b-9760-50d30a6db6c4', // the resource id
-	//     limit: 15 // get 15 results
-	//   };
-	//   $.ajax({
-	//     url: 'http://data.obshtestvo.bg/api/action/datastore_search',
-	//     data: data,
-	//     dataType: 'jsonp',
-	//     success: function(data) {
-	//       alert('Total results found: ' + data.result.total)
-	//     },
-	//     error: function(data,text) {
-	//     	alert(JSON.stringify(data));
-	//     	alert(text);
-	//     }
-	//   });
+    var sofia = {lat: 42.637626, lng: 23.322284}; //initialize with Sofia coordinates
+    var showMap = function(center) {
+        var mapOptions = { center: center, zoom: 12 };
+        return new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+    }
 
-    $.get('http://localhost:4000/pharmacies',function(data) {
-        debugger
-        alert(data);
-    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
 
-	var mapOptions = {
-      center: { lat: 42.637626, lng: 23.322284},
-      zoom: 12
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+                var location = {lat: position.coords.latitude, lng: position.coords.longitude};
+                var map = showMap(location);
 
-	
-
-	// var addresses = $.map(dataset, function(item) {
-	// 	return item["Адрес на аптека"];
-	// });
-
-
-	// var marker = new google.maps.Marker({
- //        map: map,
- //        position: results[0].geometry.location,
- //        title: address
- //    });
+                $.get('http://localhost:4000/pharmacies?lat='+location.lat+'&lng='+location.lng,
+                    function(data) {
+                        //debugger
+                        $.each(data.results,function(idx,item) {
+                            var marker = new google.maps.Marker({
+                                map: map,
+                                position: new google.maps.LatLng(item.latitude,item.longitude),
+                                title: item["Адрес на аптека"]
+                            });
+                        });
+                    }
+                );
+            },
+            function() { showMap(sofia); } //error
+        );
+    }
+    else {
+        showMap(sofia);
+    }
 
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyparser = require('body-parser');
 var http = require('http');
+var querystring = require('querystring');
 
 var app = express();
 app.use(bodyparser.json());
@@ -17,11 +18,19 @@ app.get('/medicines', function(req,res) {
 
 app.get('/pharmacies', function(req,res) {
 
+	//we need to limit the precision because the query characters are limited
+	var lat = +parseFloat(req.query.lat).toFixed(4);
+	var lng = +parseFloat(req.query.lng).toFixed(4);
+	var interval = 0.002;
+	var sql = querystring.escape('SELECT "Адрес на аптека",latitude,longitude ' + 
+								'from "1e8cfdf8-65cd-4bda-80e3-9ec1cf5a1c09" WHERE ' + 
+								'latitude BETWEEN ' + (lat-interval) + ' AND ' + (lat+interval) + 
+								' AND longitude BETWEEN ' + (lng-interval) + ' AND ' + (lng+interval));
+
 	var options = {
 	  host: 'data.obshtestvo.bg',
 	  port: 80,
-	  //More than 2500 requests will reach the API limit for Google Geocoding
-	  path: '/api/action/datastore_search?resource_id=3d38a652-adbc-442b-9760-50d30a6db6c4'
+	  path: '/api/action/datastore_search_sql?sql=' + sql
 	};
 
 	http.get(options, function(ckan_res) {
@@ -30,10 +39,17 @@ app.get('/pharmacies', function(req,res) {
 	    body += chunk;
 	  });
 	  ckan_res.on('end', function() {
-	    res.send(JSON.parse(body).result.records);
+	  	var parsed = JSON.parse(body);
+	  	if (parsed.success)
+	  		res.status(200).send({"results":parsed.result.records});
+	  	else {
+	  		console.log(parsed.error);
+	  		res.status(409).send({"error":parsed.error});
+	  	}	
 	  });
 	}).on('error', function(e) {
 	  console.log("Error: " + JSON.stringify(e));
+	  res.status(409).send({"error":e});
 	});
 
 });
